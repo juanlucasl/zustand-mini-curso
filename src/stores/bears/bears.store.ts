1be02dd3d4886f67f56c2b9d7f8d41aa41a452db
Juan L <33398520+juanlucasl@users.noreply.github.com>
2024-09-01 21:12:56 -0300

    Implement totalBears calculation

--- src/stores/bears/bears.store.ts
+++ src/stores/bears/bears.store.ts
@@ -1,21 +1,51 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 export const enum BearSpecies {
-  Black = 'black',
-  Panda = 'panda',
-  Polar = 'polar'
+  Black = "black",
+  Panda = "panda",
+  Polar = "polar"
+}
+
+interface Bear {
+  id: number;
+  name: string;
 }
 
 type BearState = {
   [key in BearSpecies]: number;
 } & {
-  updateBears: (type: BearSpecies, by: number) => void; removeAllBears: () => void;
+  bears: Bear[],
+  computed: {
+    totalBears: number;
+  }
+  updateBears: (type: BearSpecies, by: number) => void;
+  resetBearsCount: () => void;
+  addBear: () => void;
+  clearBears: () => void;
 };
 
-export const useBearStore = create<BearState>()((set) => ({
+export const useBearStore = create<BearState>()((set, get) => ({
   [BearSpecies.Black]: 10,
   [BearSpecies.Panda]: 1,
   [BearSpecies.Polar]: 5,
+  bears: [{ id: 1, name: "Bear #1" }],
+
+  computed: {
+    get totalBears(): number {
+      return get()[BearSpecies.Black] + get()[BearSpecies.Panda] + get()[BearSpecies.Polar] + get().bears.length
+    }
+  },
+
   updateBears: (type, by) => set((state) => ({ [type]: state[type] + by })),
-  removeAllBears: () => set({ [BearSpecies.Black]: 0, [BearSpecies.Panda]: 0, [BearSpecies.Polar]: 0 })
+  resetBearsCount: () => set({ [BearSpecies.Black]: 0, [BearSpecies.Panda]: 0, [BearSpecies.Polar]: 0 }),
+  addBear: () => set(state => {
+    const nextBearId = state.bears.length + 1;
+    return ({
+      bears: [...state.bears, {
+        id: nextBearId,
+        name: `Bear #${nextBearId}`
+      }]
+    });
+  }),
+  clearBears: () => set({ bears: [] })
 }));
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,1 +1,1 @@
-export * from './bears/bears.store.ts';
+export * from "./bears/bears.store.ts";
