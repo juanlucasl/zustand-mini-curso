0845f37128fecc896851b47e95794d895d8a492f
Juan L <33398520+juanlucasl@users.noreply.github.com>
2024-09-07 19:58:43 -0300

    Implement Person Store

--- src/pages/01-basic/PersonPage.tsx
+++ src/pages/01-basic/PersonPage.tsx
@@ -1,8 +1,12 @@
-import { WhiteCard } from '../../components';
-
-
+import { WhiteCard } from "../../components";
+import { usePersonStore } from "../../stores";
 
 export const PersonPage = () => {
+  const firstName = usePersonStore(state => state.firstName);
+  const setFirstName = usePersonStore(state => state.setFirstName)
+  const lastName = usePersonStore(state => state.lastName);
+  const setLastName = usePersonStore(state => state.setLastName)
+
   return (
     <>
       <h1>Persona</h1>
@@ -18,13 +22,15 @@ export const PersonPage = () => {
                   <label
                     className="mb-3 block text-base font-medium text-[#07074D]"
                   >
-                    Primer Nombre
+                    Nombre
                   </label>
                   <input
                     type="text"
                     name="firstName"
                     id="firstName"
-                    placeholder="Primer Nombre"
+                    placeholder="Nombre"
+                    value={firstName}
+                    onChange={(event) => setFirstName(event.target.value)}
                   />
                 </div>
               </div>
@@ -40,6 +46,8 @@ export const PersonPage = () => {
                     name="lastName"
                     id="lastName"
                     placeholder="Apellido"
+                    value={lastName}
+                    onChange={(event) => setLastName(event.target.value)}
                   />
                 </div>
               </div>
@@ -48,8 +56,8 @@ export const PersonPage = () => {
             <pre className="bg-gray-200 p-5 rounded-[20px]">
               {
                 JSON.stringify({
-                  firstName: '',
-                  lastName: ''
+                  firstName,
+                  lastName
                 }, null, 2)
               }
             </pre>
